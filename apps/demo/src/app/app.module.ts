1f6a4160c3fa5582543b56683782b6072ffd8820
Julian Pömp <jp@poemp.net>
2024-01-12 09:45:49 +0100

    docs(ngx-jodit): improved demo page

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {AppComponent} from './app.component';
 import {NgxJoditModule} from 'ngx-jodit';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, NgxJoditModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
