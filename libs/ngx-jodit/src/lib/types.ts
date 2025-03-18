bbb08b1c8e7ba5140a265198a3ab3cb125c52978
Julian Pömp <jp@poemp.net>
2025-03-18 22:00:08 +0100

    fix(ngx-jodit): try to fox IViewOptions issue ref #37

--- libs/ngx-jodit/src/lib/types.ts
+++ libs/ngx-jodit/src/lib/types.ts
@@ -1,7 +1,10 @@
-import type {IViewOptions} from 'jodit/types';
+import type {IViewOptions} from 'jodit/types/types';
 import * as JoditESMConfig from 'jodit/config';
 import * as JoditCoreConfig from 'jodit/types/config';
 
-export type JoditConfig = Partial<Record<string, any> &
-  IViewOptions & JoditCoreConfig.Config & JoditESMConfig.Config
+export type JoditConfig = Partial<
+  Record<string, any> &
+    IViewOptions &
+    JoditCoreConfig.Config &
+    JoditESMConfig.Config
 >;
